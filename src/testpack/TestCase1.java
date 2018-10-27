d368ece1821fb345705f52f043d2f4fc0aba44ff
syedsatham <syed020692@gmail.com>
2018-10-27 11:52:05 +0530

    test case 1 work by mani

---
+++ src/testpack/TestCase1.java
@@ -0,0 +1,13 @@
+package testpack;
+
+public class TestCase1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void add(int a, int b, int c) {
+		System.out.println(a + b + c);
+	}
+}
