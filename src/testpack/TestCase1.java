4e4a509ddc1b33d329798ac737a12650f480576a
syedsatham <syed020692@gmail.com>
2018-10-27 11:47:31 +0530

    completed test case1 by syed

---
+++ src/testpack/TestCase1.java
@@ -0,0 +1,13 @@
+package testpack;
+
+public class TestCase1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void add(int a, int b) {
+		System.out.println(a + b);
+	}
+}
